e2d88cb322b545d8216226f19a16255ba83bf3b2
Sam Ghamian <sam@oussama.localdomain>
2020-02-14 17:26:57 +0200

    todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      currentTask: "",
+      tasksList: []
+    }
+  }
+  /**
+   * write your functions between the constructor and the function "render".
+   */
+
+  hundleAdd = async (e) => {
+    if(this.state.tasksList.length > 0)
+    await this.setState({
+      tasksList: [...this.state.tasksList, this.state.currentTask]
+    })
+
+  }
+  hundleInput = (e) => {
+    console.log(e.target.value)
+    if(e.target.value !== "")
+    this.setState({ currentTask: e.target.value })
+    
+
+  }
+  render() {
+    return (
+
+      <div className="App">
+        <h1>Todo App</h1>
+        <input type="text" placeholder="add a task..." onChange={this.hundleInput} />
+        <input type="button" value="Add" onClick={this.hundleAdd} />
+        <ul>
+          {this.state.tasksList.map((task, index) => <li key={index}>{task}</li>)}
+        </ul>
+      </div>
+
+    );
+  }
 }
 
-export default App;
