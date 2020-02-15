9de48cb53d22c04debd46f8f18976a420b531ca6
Sam Ghamian <sam@oussama.localdomain>
2020-02-15 11:54:30 +0200

    todo

--- src/App.js
+++ src/App.js
@@ -14,28 +14,40 @@ export default class App extends React.Component {
    */
 
   hundleAdd = async (e) => {
-    if(this.state.tasksList.length > 0)
-    await this.setState({
-      tasksList: [...this.state.tasksList, this.state.currentTask]
-    })
-
+    if (this.state.currentTask != "")
+      await this.setState({
+        tasksList: [...this.state.tasksList, this.state.currentTask],
+        currentTask: ""
+      })
   }
   hundleInput = (e) => {
-    console.log(e.target.value)
-    if(e.target.value !== "")
     this.setState({ currentTask: e.target.value })
-    
+  }
 
+  handleDelete = (e) => {
+
+    this.setState({
+      tasksList: this.state.tasksList.filter((task, index) => index != e.target.value)
+    })
+  }
+  handleEdit = () => {
+    
   }
   render() {
     return (
 
       <div className="App">
         <h1>Todo App</h1>
-        <input type="text" placeholder="add a task..." onChange={this.hundleInput} />
+        <input type="text" placeholder="add a task..." value={this.state.currentTask} onChange={this.hundleInput} />
         <input type="button" value="Add" onClick={this.hundleAdd} />
         <ul>
-          {this.state.tasksList.map((task, index) => <li key={index}>{task}</li>)}
+          {this.state.tasksList.map((task, index) => <div className="item">
+            <li >{task}</li>
+            <div className="btnGrp">
+              <button value={index} onClick={this.handleDelete}>Del</button>
+              <button value={index} onClick={this.handleEdit}>Edit</button>
+            </div>
+          </div>)}
         </ul>
       </div>
 
